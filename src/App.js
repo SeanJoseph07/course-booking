a70e29daf33aed1995b485377cf2ca69513ea584
SeanJoseph <josephseankyle@gmail.com>
2022-05-30 17:23:34 +0800

    added AddCourse AdminView and UserView

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import AppNavbar from './components/AppNavbar';
 import Home from './pages/Home';
@@ -6,7 +6,9 @@ import CoursePage from './pages/CoursePage';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Logout from './pages/Logout';
+import NotFound from './pages/NotFound';
 import { Container } from 'react-bootstrap';
+import { UserProvider } from './UserContext';
 
 //react-router
 //BrowserRouter as Router(alternative)
@@ -14,19 +16,35 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //<>..</> (Fragment) needs to add if we added multiple components/html tags
 export default function App() {
+
+    //state hook for the user state that defined here for global scope
+    //This will be used to store the user information and will be used for validating if a user is logged in on the app or not.
+    const [ user, setUser ] = useState({
+        accessToken: localStorage.getItem('accessToken'),
+        isAdmin: localStorage.getItem('isAdmin') === 'true'
+    })
+
+    //function for clearing localStorage on logout
+    const unsetUser = () => {
+        localStorage.clear();
+    }
+
   return (
-    <BrowserRouter>
-        <AppNavbar />
-        <Container>
-            <Routes>
-                <Route  path="/" element={ <Home /> }/>
-                <Route  path="/courses" element={ <CoursePage /> }/>
-                <Route  path="/register" element={ <Register /> }/>
-                <Route  path="/login" element={ <Login /> }/>
-                <Route  path="/logout" element={ <Logout /> }/>
-            </Routes>
-        </Container>
-    </BrowserRouter>
+    <UserProvider value = {{ user, setUser, unsetUser }} >
+        <BrowserRouter>
+            <AppNavbar />
+            <Container>
+                <Routes>
+                    <Route  path="/" element={ <Home /> }/>
+                    <Route  path="/courses" element={ <CoursePage /> }/>
+                    <Route  path="/register" element={ <Register /> }/>
+                    <Route  path="/login" element={ <Login /> }/>
+                    <Route  path="/logout" element={ <Logout /> }/>
+                    <Route path="*" element={ <NotFound /> } />
+                </Routes>
+            </Container>
+        </BrowserRouter>
+    </UserProvider>
   );
 }
 
--- src/pages/CoursePage.js
+++ src/pages/CoursePage.js
@@ -1,4 +1,51 @@
-import CourseCard from '../components/CourseCard';
+import UserView from '../components/UserView';
+import AdminView from '../components/AdminView';
+import { useContext, useEffect, useState } from 'react';
+import UserContext from '../UserContext';
+
+
+export default function CoursePage() {
+
+	const [ allCourses, setAllCourses ] = useState([])
+
+	const fetchData = () => {
+		fetch('http://localhost:4000/courses/all')
+		.then(res => res.json())
+		.then(data => {
+			console.log(data)
+			//storing all the data to our useState allCourses
+			setAllCourses(data)
+		})
+	}
+
+	//render
+	useEffect(() => {
+		fetchData()
+	}, [])
+
+	const { user } = useContext(UserContext);
+
+	return(
+		<>
+			<h1>Courses</h1>
+
+			{(user.isAdmin === true) ? 
+				<AdminView coursesData={allCourses} fetchData={fetchData} />
+
+				:
+
+				<UserView coursesData={allCourses} />
+			}
+
+		</>
+
+
+		)
+}
+
+
+
+/*import CourseCard from '../components/CourseCard';
 import coursesData from '../mockData/coursesData';
 
 export default function CoursePage() {
@@ -25,4 +72,4 @@ export default function CoursePage() {
 
 
 		)
-}
+}*/
